121157f9d90f2cd347e63b6746a243aa24bc5ec9
stakenschneider <volkova.maria.d@gmail.com>
2020-09-11 17:28:23 +0300

    add day 13

---
+++ src/main/java/HackerRank/thirtyDaysOfCode/Day_13.java
@@ -0,0 +1,57 @@
+package hackerRank.thirtyDaysOfCode;
+
+import java.util.*;
+
+// Abstract Classes
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book{
+    int price;
+
+    /**
+     *   Class Constructor
+     *
+     *   @param title The book's title.
+     *   @param author The book's author.
+     *   @param price The book's price.
+     **/
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    /**
+     *   Method Name: display
+     *
+     *   Print the title, author, and price in the specified format.
+     **/
+    @Override
+    void display() {
+        System.out.print("Title: "+title+"\n" +
+                "Author: "+author+"\n" +
+                "Price: "+price);
+    }
+}
+
+class Day_13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
