e37f1c77c2f9a58f56460e364485689e58ed53b7
stakenschneider <volkova.maria.d@gmail.com>
2020-09-15 12:32:03 +0300

    add day 20

---
+++ src/main/java/HackerRank/thirtyDaysOfCode/Day_20.java
@@ -0,0 +1,34 @@
+package hackerRank.thirtyDaysOfCode;
+
+import java.util.*;
+
+// Sorting
+public class Day_20 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int numberOfSwaps = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int s = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = s;
+                    numberOfSwaps++;
+                }
+            }
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in "+numberOfSwaps+" swaps.\n" +
+                "First Element: "+a[0]+"\n" +
+                "Last Element: "+a[a.length-1]);
+    }
+}
