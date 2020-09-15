538b3ccca3a85c1c8b42c4b0aa837aadb3c4b800
stakenschneider <volkova.maria.d@gmail.com>
2020-09-15 12:31:57 +0300

    add day 19

---
+++ src/main/java/HackerRank/thirtyDaysOfCode/Day_19.java
@@ -0,0 +1,35 @@
+package hackerRank.thirtyDaysOfCode;
+
+import java.util.*;
+
+// Interfaces
+interface AdvancedArithmetic {
+    int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+
+    @Override
+    public int divisorSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += + i;
+            }
+        }
+        return sum + n;
+    }
+}
+
+class Day_19 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName());
+        System.out.println(sum);
+    }
+}
