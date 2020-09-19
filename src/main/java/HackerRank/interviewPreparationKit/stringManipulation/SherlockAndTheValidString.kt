d14243ce1dde64f0a1683056187b15ed9bef8924
stakenschneider <volkova.maria.d@gmail.com>
2020-09-20 00:03:59 +0300

    add Sherlock and the valid string - medium

---
+++ src/main/java/HackerRank/interviewPreparationKit/stringManipulation/SherlockAndTheValidString.kt
@@ -0,0 +1,31 @@
+package hackerRank.interviewPreparationKit.stringManipulation
+
+import java.util.*
+
+fun isValid(s: String): String? {
+    val letters = IntArray(26)
+    for (element in s) {
+        letters[element - 'a']++
+    }
+
+    Arrays.sort(letters)
+    var i = 0
+
+    while (letters[i] == 0) {
+        i++
+    }
+
+    val min = letters[i]
+    val max = letters[25]
+    var ret = "NO"
+
+    if (min == max) ret = "YES" else {
+        if (max - min == 1 && max > letters[24] ||
+                min == 1 && letters[i + 1] == max) ret = "YES"
+    }
+    return ret
+}
+
+fun main(args: Array<String>) {
+    println(isValid("ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"))
+}
