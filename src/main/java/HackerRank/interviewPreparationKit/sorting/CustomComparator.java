97b0fe5ab552551fde2fa1f5843950ed8dd8f60b
stakenschneider <volkova.maria.d@gmail.com>
2020-09-30 03:00:39 +0300

    add Comparator - easy

---
+++ src/main/java/HackerRank/interviewPreparationKit/sorting/CustomComparator.java
@@ -0,0 +1,42 @@
+package hackerRank.interviewPreparationKit.sorting;
+
+import java.util.*;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if(a.score>b.score) return -1;
+        else if(a.score<b.score) return 1;
+        else return a.name.compareTo(b.name);
+    }
+}
+
+public class CustomComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (Player value : player) {
+            System.out.printf("%s %s\n", value.name, value.score);
+        }
+    }
+}
